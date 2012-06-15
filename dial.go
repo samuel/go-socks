c32f5e84b9045457647f69b2492d668d04e12e5e
Samuel Stauffer <samuel@descolada.com>
2012-06-14 23:50:06 -0700

    Add an example http client over SOCKS5

--- dial.go
+++ dial.go
@@ -1,11 +1,20 @@
 /*
-	Current limitations:
+Current limitations:
 
 	- GSS-API authentication is not supported
-
 	- only SOCKS version 5 is supported
-
 	- TCP bind and UDP not yet supported
+
+Example http client over SOCKS5:
+
+	proxy := &socks.Proxy{"127.0.0.1:1080"}
+	tr := &http.Transport{
+		Dial: func(net, addr string) (net.Conn, error) {
+			return proxy.Dial(net, addr)
+		},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get("https://example.com")
 */
 package socks
 
