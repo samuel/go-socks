9e9d1f5fd4fc75b179a736fe26ba316f7d191c1c
David Hill <dhill@mindcry.org>
2016-12-14 16:12:04 -0500

    Add DialTimeout API

--- socks/dial.go
+++ socks/dial.go
@@ -27,6 +27,7 @@ import (
 	"io"
 	"net"
 	"strconv"
+	"time"
 )
 
 const (
@@ -83,6 +84,14 @@ type Proxy struct {
 }
 
 func (p *Proxy) Dial(network, addr string) (net.Conn, error) {
+	return p.dial(network, addr, nil)
+}
+
+func (p *Proxy) DialTimeout(network, addr string, timeout time.Duration) (net.Conn, error) {
+	return p.dial(network, addr, &timeout)
+}
+
+func (p *Proxy) dial(network, addr string, timeout *time.Duration) (net.Conn, error) {
 	host, strPort, err := net.SplitHostPort(addr)
 	if err != nil {
 		return nil, err
@@ -92,7 +101,12 @@ func (p *Proxy) Dial(network, addr string) (net.Conn, error) {
 		return nil, err
 	}
 
-	conn, err := net.Dial("tcp", p.Addr)
+	var conn net.Conn
+	if timeout != nil {
+		conn, err = net.DialTimeout("tcp", p.Addr, *timeout)
+	} else {
+		conn, err = net.Dial("tcp", p.Addr)
+	}
 	if err != nil {
 		return nil, err
 	}
